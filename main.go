516ea29a25e5f981d100e25110c7910e4bbf0836
Ridwan Abdullah <teambacen@gmail.com>
2017-10-11 12:47:55 +0800

    adding rollbar

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-packgae main
+package main
+
+import (
+	"github.com/stvp/rollbar"
+)
+
+func main() {
+	rollbar.Token = "d9b441c1b032459eaa34ebbcfe7ed232"
+	rollbar.Environment = "production" // defaults to "development"
+
+	// Message reporting
+	rollbar.Message("info", "Message body goes here")
+
+	// Block until all queued messages are sent to Rollbar.
+	// You can do this in a defer() if needed.
+	rollbar.Wait()
+}
